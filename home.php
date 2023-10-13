8ba7bdd8bae45dbd9578a42c0a0d99dff609b32f
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-10-13 18:21:29 +0530

    commit1

---
+++ home.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imagination</title>
+     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+    <!--style css link-->
+<link rel="stylesheet" href="style.css">
+    
+    <!--boostrap css link-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!--font auwsom link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+  <!--navbar-->
+  <header class="header">
+  <div class="container-fluid p-0">
+    <!--first child-->
+    <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid bg-black">
+   <img src="./images/Logo.jpeg" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link-home active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Stories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total Price</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex search-item" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </form>
+    </div>
+  </div>
+</nav>
+  </div>
+  </header>
+  <!--home section start-->
+  
+   <!--third child-->
+   <div class="bg-light">
+    <h3 class=" text-center">All Categories</h3>
+    <p class=" text-center">latest stories</p>
+   </div>
+
+    <!--fourth child-->
+    <div class="row">
+          <!--sidenav-->
+<div class="col-md-2 bg-secondary">
+  <ul class="navbar-nav me-auto">
+    <li class="nav-item bg-info">
+      <a href="" class="nav-link text-light"><h4>Family</h4></a>
+    </li>
+      <li class="nav-item bg-info">
+      <a href="" class="nav-link text-light"><h4>Fantacy</h4></a>
+    </li>
+      <li class="nav-item bg-info">
+      <a href="" class="nav-link text-light"><h4>Comedy</h4></a>
+    </li>
+      <li class="nav-item bg-info">
+      <a href="" class="nav-link text-light"><h4>Thirller</h4></a>
+    </li>
+      <li class="nav-item bg-info">
+      <a href="" class="nav-link text-light"><h4>Horror</h4></a>
+    </li>
+  </ul>
+</div>
+      
+ <!--products-->
+ <div class="col-md-10">
+<div class="row">
+  <div class="col-md-4 mb-2">
+    <div class="card" style="width: 18rem;">
+  <img src="./images/ts1.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+     <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+  </div>
+   <div class="col-md-4"><div class="card" style="width: 18rem;">
+  <img src="./images/ts2.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+    <div class="col-md-4"><div class="card" style="width: 18rem;">
+  <img src="./images/ts3.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+</div>
+<!--row2-->
+<div class="row">
+  <div class="col-md-4 mb-2">
+    <div class="card" style="width: 18rem;">
+  <img src="./images/ts1.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+   <div class="col-md-4"><div class="card" style="width: 18rem;">
+  <img src="./images/ts2.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+    <div class="col-md-4"><div class="card" style="width: 18rem;">
+  <img src="./images/ts3.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+</div>
+      </div>
+   
+    </div>
+    </div>
+ <!--last child-->
+ <div class="bg-info p-3 text-csnter">
+  All rights
+ </div>
+  <!--boostrap css link-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<!--custom js file link--> 
+<script src="script.js"></script>
+
+
+</body>
+</html>
