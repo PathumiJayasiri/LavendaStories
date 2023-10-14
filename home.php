bb124d99644d8f51dcb3966745120e9abcd8593c
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-10-14 14:50:02 +0530

    home

--- home.php
+++ home.php
@@ -13,15 +13,15 @@
     <!--font auwsom link-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
+<body class="bg-black">
   <!--navbar-->
   <header class="header">
   <div class="container-fluid p-0">
     <!--first child-->
     <nav class="navbar navbar-expand-lg">
-  <div class="container-fluid bg-black">
+  <div class="container-fluid ">
    <img src="./images/Logo.jpeg" alt="" class="logo">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -29,14 +29,15 @@
         <li class="nav-item">
           <a class="nav-link-home active" aria-current="page" href="#">Home</a>
         </li>
+        
         <li class="nav-item">
-          <a class="nav-link" href="#">Stories</a>
+          <a class="nav-link" href="#about">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Register</a>
+          <a class="nav-link" href="#category">categories</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link" href="#contact">Contact</a>
         </li>
 
         <li class="nav-item">
@@ -47,8 +48,8 @@
         </li>
         
       </ul>
-      <form class="d-flex search-item" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <form class="d-flex search-form" role="search">
+        <input class="form-control me-2 search-item" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-light" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
       </form>
     </div>
@@ -57,16 +58,36 @@
   </div>
   </header>
   <!--home section start-->
-   <!--third child-->
-   <div class="bg-light">
-    <h3 class=" text-center">All Categories</h3>
-    <p class=" text-center">latest stories</p>
-   </div>
+<section class="home" id="home">
+  <div class="content">
+    <h3>Imagination</h3>
+    <p>Free your self and share your story with the world</p>
+   <a href="#" class="btn bg-info">View more</a>
+  </div>
+</section>
+   <!--home section end-->
+     <!--about section start-->
+<section class="about" id="about">
+  <h1 class="heading">
+    <span>about</span> us
+  </h1>
+  <div class="row">
+  <div class="image">
+    <img src="./images/bg1.jpg" alt="">
 
+  </div>
+  <div class="content">
+    <h3>Why IMAGINATION?</h3>
+    <p>"This website is dedicated to reaching out to individuals who are grappling with loneliness and engage in crafting creative, imaginative stories as a means to release their inner emotions and share them with the world. It serves as a safe haven where they can transform their feelings into words, allowing their unique narratives to resonate with a global audience. This platform empowers them to express themselves and connect with others who may appreciate their creativity and experiences."</p>
+  </div>
+  </div>
+</section>
+  <!--about section ends-->
     <!--fourth child-->
+     <section class="category" id="category">
     <div class="row">
           <!--sidenav-->
-<div class="col-md-2 bg-secondary">
+<!--<div class="col-md-2 bg-secondary">
   <ul class="navbar-nav me-auto">
     <li class="nav-item bg-info">
       <a href="" class="nav-link text-light"><h4>Family</h4></a>
@@ -84,80 +105,103 @@
       <a href="" class="nav-link text-light"><h4>Horror</h4></a>
     </li>
   </ul>
-</div>
+</div>-->
       
- <!--products-->
- <div class="col-md-10">
+ <!--category  section-->
+ 
+<h1 class="heading">Our <span>Categories</span> </h1>
+ 
+ 
 <div class="row">
-  <div class="col-md-4 mb-2">
-    <div class="card" style="width: 18rem;">
+  <div class="col-md-4 mb-2 card-container">
+    <div class="card">
   <img src="./images/ts1.jpeg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h3 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-info">Add to cart</a>
-     <a href="#" class="btn btn-secondary">View more</a>
+     <a href="#" class="btn btn-secondary">Read more</a>
   </div>
 </div>
   </div>
-   <div class="col-md-4"><div class="card" style="width: 18rem;">
-  <img src="./images/ts2.jpeg" class="card-img-top" alt="...">
+  <div class="col-md-4 mb-2 card-container">
+    <div class="card">
+  <img src="./images/ts1.jpeg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h3 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+     <a href="#" class="btn btn-secondary">Read more</a>
+
   </div>
 </div>
-</div>
-    <div class="col-md-4"><div class="card" style="width: 18rem;">
+  </div>
+
+    <div class="col-md-4 mb-2 card-container"><div class="card" >
   <img src="./images/ts3.jpeg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h3 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+     <a href="#" class="btn btn-secondary">Read more</a>
+
   </div>
 </div>
 </div>
 </div>
 <!--row2-->
 <div class="row">
-  <div class="col-md-4 mb-2">
-    <div class="card" style="width: 18rem;">
+  <div class="col-md-4 mb-2 card-container">
+    <div class="card" >
   <img src="./images/ts1.jpeg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h3 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+     <a href="#" class="btn btn-secondary">Read more</a>
+
   </div>
 </div>
   </div>
-   <div class="col-md-4"><div class="card" style="width: 18rem;">
+   <div class="col-md-4 card-container"><div class="card" >
   <img src="./images/ts2.jpeg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h3 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+     <a href="#" class="btn btn-secondary">Read more</a>
+
   </div>
 </div>
 </div>
-    <div class="col-md-4"><div class="card" style="width: 18rem;">
+    <div class="col-md-4 card-container"><div class="card" >
   <img src="./images/ts3.jpeg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h3 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+       <a href="#" class="btn btn-secondary">Read more</a>
+
   </div>
 </div>
 </div>
 </div>
-      </div>
+    
    
     </div>
     </div>
- <!--last child-->
- <div class="bg-info p-3 text-csnter">
-  All rights
+     </section>
+ <!--footer-->
+ <section class="footer">
+ <div class="share">
+  <a href="#" class="fab fa-facebook"></a>
+   <a href="#" class="fab fa-twitter"></a>
+    <a href="#" class="fab fa-instagram"></a>
+     <a href="#" class="fab fa-linkedin"></a>
  </div>
+ <div class="links">
+  <a href="#">home</a>
+  <a href="#">category</a>
+  <a href="#">about us</a>
+  <a href="#">contact us</a>
+ </div>
+   <div class="credit">created by <span>IMAGINATION web designer</span> |all rights reserved</div>
+
+ </section>
   <!--boostrap css link-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <!--custom js file link--> 
