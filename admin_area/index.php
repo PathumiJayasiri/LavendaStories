87b0f68e94e2a2b34be8d8ebfab2749ae270ea6a
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-10-16 21:17:04 +0530

    incorrect

--- admin_area/index.php
+++ admin_area/index.php
@@ -7,7 +7,12 @@
      <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
     <!--style css link-->
 <link rel="stylesheet" href="../style.css">
-    
+    <link rel="stylesheet" href="style_insert_story.css">
+
+
+<!--add CKEDITOR library-->
+<script src="https://cdn.ckeditor.com/4.16.2/full/ckeditor.js"></script>
+
     <!--boostrap css link-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!--font auwsom link-->
@@ -71,7 +76,8 @@
 <div class="container my-5">
     <?php
     if(isset($_GET['insert_story'])){
-        include('insert_story.php');
+
+       include('insert_story.php');
     }
     ?>
 </div>
--- admin_area/insert_story.php
+++ admin_area/insert_story.php
@@ -4,12 +4,12 @@ include('../DbConnector/connect.php');
 
 
 //if form is submitted
-if(isset($_POST['insert_infor'])){
+if(isset($_POST['submit'])){
     $writter_name=$_POST['writter_name'];
     $story_tit = $_POST['story_title']; // Corrected to 'story_title'
     $story_descri = $_POST['story_description']; // Corrected to 'story_descri'
     $story_cat = $_POST['story_category'];
-
+$editorContent=$_POST['editor'];
     // File handling for image upload
     if (!empty($_FILES['story_img']['name'])) {
         $story_img1 = $_FILES['story_img']['name'];
@@ -21,16 +21,16 @@ if(isset($_POST['insert_infor'])){
     }
 
 //check where empty
-    if(!empty($writer_name)||!empty($story_tit)||!empty($story_descri)||!empty($story_cat)||!empty($story_img1)){
+    if(!empty($writer_name)||!empty($story_tit)||!empty($story_descri)||!empty($story_cat)||!empty($story_img1)||!empty($editorContent)){
 
 //move_uploaded_file($tem_story_img,"./story_cover_images/$story_img");
 
-        $insert_story=("INSERT INTO `story` (writter_name,story_title,story_description,category_id,cover_image,date) VALUES ('$writter_name','$story_tit','$story_descri','$story_cat','$story_img1',NOW()) ");
+        $insert_story=("INSERT INTO `view_stories` (writter_name,story_title,story_description,category_id,cover_image,content,created) VALUES ('$writter_name','$story_tit','$story_descri','$story_cat','$story_img1','$editorContent',NOW()) ");
         $result_query=mysqli_query($con,$insert_story);
 
         if($result_query){
-                   //echo "<script>alert('successfully inserted the story')</script>";
-                   header("Location:story_write.php");
+                   echo "<script>alert('successfully inserted the story')</script>";
+                   
 
         }else{
                    echo "<script>alert('not successfully inserted the story')</script>";
@@ -45,11 +45,18 @@ if(isset($_POST['insert_infor'])){
 
 ?>
 
-
+<div class="container">
         <h1 class="text-center bg-info">Write your Story</h1>
+
 
         <!--writing story-->
          <form action="" method="POST" enctype="multipart/form-data" class="border" style="color: wheat;">
+<div style="text-align:center;margin-top:40px;">
+  <span class="step"></span>
+  <span class="step"></span>
+  
+</div>
+<div class="tab">
 <div class="form-outline mb-4 w-50 m-auto">
       <!--title-->
     <label for="writter_name" class="form-label">
@@ -101,18 +108,96 @@ while($row=mysqli_fetch_assoc($result_query)){
     <input type="file" name="story_img" id="story_img" class="form-control" required>
  </div>
   
+</div>
+<!--textarea-->
+<div class="tab">
 
-
-
+<div class="form-outline mb-4 w-50 m-auto">
 
          
-        
-          <div class="form-outline mb-4 w-50 m-auto">
-            <input type="submit" name="insert_infor" value="Submit" class="btn btn-outline-success btn-info mb-3 px-3">
-            
-   </div>
-         </form>
-    
+            <textarea name="editor" id="editor" cols="80" rows="10">
+                this is my text area
+            </textarea>
+</div>
 
+</div>
+
+<!--move button-->
+        <div style="overflow: hidden;">
+        <div style="float: right;">
+        <div style="overflow:auto;">
+    <div style="float:right;">
+      <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+      <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+    </div>
+  </div>
+    </div>
+    </div>
+          <!--div class="form-outline mb-4 w-50 m-auto">
+            <input type="submit" name="submit" value="Submit" class="btn btn-outline-success btn-info mb-3 px-3">
+            
+   </div-->
+         </form>
+</div>
+<script>
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
+</script>
+ <!--add CKEDITOR to textarea-->
+ <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            CKEDITOR.replace('editor');
+        });
+    </script>
            
    
