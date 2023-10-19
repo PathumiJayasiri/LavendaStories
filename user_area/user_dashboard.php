c2fafa029ef17ddead484646c19cd6f20451cdde
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-10-19 09:30:17 +0530

    commit15

--- user_area/user_dashboard.php
+++ user_area/user_dashboard.php
@@ -6,7 +6,7 @@ include('../DbConnector/connect.php');
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imagination</title>
      <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
     <!--style css link-->
