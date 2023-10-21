11168ef692eee8434e8c44a18acf25ba0714d8fb
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-10-21 22:14:23 +0530

    de2

---
+++ user_area/delete_user_acc.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+    <form action="" method="post" class="mt-5 mb-5">
+        <h4 class="text-center mb-5">Are you sure you want to delete this account?</h4>
+        <div class="form-outline mb-4">
+            <input type="submit" class="form-control w-50 m-auto" name="delete" value="Yes">
+        </div>
+        <div class="form-outline mb-4">
+            <input type="submit" class="form-control w-50 m-auto" name="dont_delete" value="No">
+        </div>
+    </form>
