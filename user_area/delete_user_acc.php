7a757b6df0f24b333713ddda914f11dc11b63f21
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-10-21 22:24:40 +0530

    de3

--- user_area/delete_user_acc.php
+++ user_area/delete_user_acc.php
@@ -1,4 +1,21 @@
 <?php
+$username_session=$_SESSION['username'];
+if(isset($_POST['delete'])){
+$delete_query="delete from `user` where username='$username_session'";
+$rs=mysqli_query($con,$delete_query);
+if($rs){
+    session_destroy();
+     echo "<script>alert('Account successfully deleted')</script>";
+                                                      echo "<script>window.open('../home.php','_self')</script>";
+}
+
+}
+if(isset($_POST['dont_delete'])){
+
+echo "<script>window.open('user_index.php','_self')</script>";
+
+
+}
 
 ?>
     <form action="" method="post" class="mt-5 mb-5">
