b1113a8845993294dfc287d6e0d2c6f6f9851e1c
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-10-30 19:46:44 +0530

    c44

--- home.php
+++ home.php
@@ -13,12 +13,273 @@ session_start();
     <!--style css link-->
 <link rel="stylesheet" href="style.css">
     
+
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@100;300;500;700&family=Sen:wght@400;700;800&display=swap");
+:root {
+  --main-color: aqua;
+  --black: #13131a;
+  --bg: #010103;
+  --border: 0.1rem solid rgba(25, 255, 255, 0.3);
+}
+
+* {
+  font-family: "Roboto", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: none;
+  outline: none;
+  text-decoration: none;
+  text-transform: capitalize;
+  transition: 1ms linear;
+}
+
+html {
+  font-size: 70%;
+  overflow-x: hidden;
+  scroll-padding-top: 9rem;
+  scroll-behavior: smooth;
+}
+html::-webkit-scrollbar {
+  width: 0.8rem;
+}
+html::-webkit-scrollbar-track {
+  background: transparent;
+}
+html::-webkit-scrollbar-thumb {
+  background: #fff;
+  border-radius: 5rem;
+}
+
+section {
+  padding: 2rem 7%;
+}
+.heading {
+  text-align: center;
+  color: #fff;
+  text-transform: uppercase;
+  padding-bottom: 3.5rem;
+  font-size: 4rem;
+}
+.heading span {
+  color: var(--main-color);
+  text-transform: uppercase;
+}
+.home .content.btn {
+  margin-top: 1rem;
+  display: inline-block;
+  padding: 0.8rem 3rem;
+  font-size: 3.7rem;
+  color: #eee;
+
+  cursor: pointer;
+}
+.home .content .btn:hover {
+  letter-spacing: 0.2rem;
+}
+.header .navbar {
+  background: var(--black);
+  position: fixed;
+  padding: 1.5rem 2rem;
+  width: 100%;
+  z-index: 1000;
+  padding-top: 0%;
+}
+.logo {
+  height: 5%;
+  width: 5%;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+.header a {
+  margin: 0 1rem;
+  font-size: 1.6rem;
+  color: #fff;
+}
+.navbar a:hover {
+  color: var(--main-color);
+  border-bottom: 0.1rem solid var(--main-color);
+  padding-bottom: 0.5rem;
+}
+.navbar .nav-link-home {
+  margin: 0 1rem;
+  font-size: 1.6rem;
+
+  color: var(--main-color);
+}
+.home {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  background: url(./images/file.jpg) no-repeat;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+}
+.home .content {
+  max-width: 60rem;
+}
+.home .content p {
+  font-size: 2rem;
+  font-weight: lighter;
+  line-height: 1.8;
+  padding: 1rem 0;
+  color: #eee;
+}
+.home .content h3 {
+  font-size: 6rem;
+  text-transform: uppercase;
+  color: #fff;
+}
+.about .row {
+  display: flex;
+  align-items: center;
+  background: var(--bg);
+  flex-wrap: wrap;
+}
+.about .row .image {
+  flex: 1 1 45rem;
+}
+.about .row .image img {
+  width: 100%;
+}
+.about .row .content {
+  flex: 1 1 45rem;
+  padding: 2rem;
+}
+.about .row .content h3 {
+  font-size: 3rem;
+  color: #fff;
+}
+.about .row .content p {
+  font-size: 1.6rem;
+  color: #ccc;
+  padding: 1rem 0;
+  line-height: 1.8;
+}
+.category .card-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  grid: 1.5rem;
+  align-items: center;
+}
+.category .card-container .card {
+  padding: 4rem;
+  text-align: center;
+  border: var(--border);
+  margin-bottom: 2px;
+  background: var(--black);
+}
+.category .card-container .card img {
+  width: 100%;
+  height: 200px;
+  object-fit: contain;
+}
+.category .card-container .card h3 {
+  color: #ccc;
+  font-size: 2rem;
+  padding: 1rem 0;
+}
+.category .card-container .card p {
+  color: #eee;
+  font-size: 1rem;
+  padding: 0.5rem 0;
+}
+.category .card-container .card:hover {
+  color: var(--black);
+}
+
+.dettail-row {
+  background: var(--black);
+  padding: 5%;
+}
+.dettail-row button {
+  background: var(--main-color);
+  padding: 2rem;
+  width: 20%;
+  text-transform: capitalize;
+  font-size: 1.5rem;
+  margin: 2rem;
+}
+
+
+
+.footer .share {
+  padding: 1rem 0;
+}
+.footer .share a {
+  height: 5rem;
+  width: 5rem;
+  line-height: 5rem;
+  font-size: 2rem;
+  color: #fff;
+  border: var(--border);
+  margin: 3rem;
+  border-radius: 50%;
+}
+.footer .share a:hover {
+  background-color: var(--main-color);
+}
+.footer .links {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 2rem 0;
+  gap: 1rem;
+}
+.footer .links a {
+  padding: 0.7rem;
+}
+.footer .links a:hover {
+  background-color: var(--main-color);
+}
+.footer .credit {
+  font-size: 1rem;
+  color: #fff;
+
+  padding: 1.5rem;
+}
+.footer .credit span {
+  color: var(--main-color);
+}
+@media (max-width: 768px) {
+  .home {
+    background-position: left;
+    justify-content: center;
+    text-align: center;
+  }
+  .home.content h3 {
+    font-size: 4.5rem;
+  }
+  .home.content p {
+    font-size: 1.5rem;
+  }
+}
+@media (max-width: 450px) {
+  html {
+    font-size: 50%;
+  }
+}
+@media (max-width: 991px) {
+  html {
+    font-size: 55%;
+  }
+  .header {
+    padding: 1.5rem 2rem;
+    padding-top: 0%;
+  }
+  section {
+    padding: 2rem;
+  }
+}
+
+    </style>
     <!--boostrap css link-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!--font auwsom link-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body class="">
+<body class="bg-black">
   <!--navbar-->
   <header class="header">
   <div class="container-fluid p-0">
@@ -93,7 +354,7 @@ session_start();
   </h1>
   <div class="row ">
   <div class="image ">
-    <img src="./images/bg1.jpg" alt="" class="col-md-2">
+    <img src="./images/face-shadow-line-imagination-light-purple-lighted-mask-wallpaper-preview.jpg" alt="" class="col-md-2">
 
   </div>
   <div class="content">
