08f140de771577ba31ac87df9cbf734ce6df74db
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-11-02 00:26:26 +0530

    cat3

--- categories.php
+++ categories.php
@@ -294,7 +294,7 @@ font-family:cursive;
     <!--font auwsom link-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body style="background: var(--main-color);">
+<body style="background:url(./images/catbg2.jpg)no-repeat;object-fit:contain">
   <!--navbar-->
   <header class="header">
   <div class="container-fluid p-0">
