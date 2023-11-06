ec41d06533236351b6c3e203c3f55dbc8dbca2a0
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-11-06 12:34:38 +0530

    submenu

--- user_area/user_index.php
+++ user_area/user_index.php
@@ -389,8 +389,9 @@ echo "
 }
 
 ?>
-                  </li>      
                     </ul>
+                  </li>      
+                    
                     </li>
                                 <li class="list-items"><a href="user_index.php?display_all_stories"><i class="fa-solid fa-list"></i><label>All Stories</label></a></li>
             <li class="list-items"><a href="user_index.php?user_stories"><i class="fa-solid fa-folder-open"></i><label>My Stories</label></a></li>
@@ -528,7 +529,6 @@ echo "<div class='col-md-3 mb-2 m-5 card-container'>
     </div>
      <!--boostrap css link-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
