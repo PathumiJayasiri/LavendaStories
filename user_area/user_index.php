b2de044681e944952e4badb984b2ad50bb35eb7b
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-11-09 22:43:35 +0530

    submenu1

--- user_area/user_index.php
+++ user_area/user_index.php
@@ -1,9 +1,23 @@
+<?php
+include('../DbConnector/connect.php');
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Navbar - Html Css Javascript</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!--font auwsom link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!--add CKEDITOR library-->
+<script src="https://cdn.ckeditor.com/4.16.2/full/ckeditor.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <style>
       :root{
@@ -69,6 +83,9 @@ a{
 .header .nav ul li a.active,
 .header .nav ul li a:hover{
 	color: #f3a93d;
+}
+.header .sub-menu{
+  display: none;
 }
 .nav-toggler{
 	height: 34px;
@@ -162,12 +179,37 @@ a{
             </button>
             <nav class="nav">
                <ul>
-                  <li><a href="#" class="active">home</a></li>
-                  <li><a href="#">about</a></li>
-                  <li><a href="#">services</a></li>
-                  <li><a href="#">portfolio</a></li>
-                  <li><a href="#">testimonials</a></li>
-                  <li><a href="#">contact</a></li>
+                 <li><a class="sub-btn">Categories<i class="fa-solid fa-angle-right dropdown"></i></a>
+                    <ul class="sub-menu" style="list-style: none;">
+                        
+    <?php
+//getting category in home page
+
+    global $con;
+$select_query="Select * from `categories`";
+$result_query=mysqli_query($con,$select_query);
+//$row=mysqli_fetch_assoc($result_query);
+//echo $row['category_title'];
+while($row=mysqli_fetch_assoc($result_query)){
+  $category_id=$row['category_id'];
+ $category_title=$row['category_title'];
+   $category_image=$row['category_image'];
+echo "
+   <li class='list-items' style='list-style: none;'>
+ <a href='user_index.php?category_stories=$category_id'><label>$category_title</label></a></li>
+";
+}
+
+?>
+                    </ul>
+                  </li>      
+                    
+                    
+   <li ><a  href="user_index.php?display_all_stories">All Stories</a></li>
+            <li ><a  href="user_index.php?user_stories">My Stories</a></li>
+            <li><a href="user_index.php?insert_story">Write Story</a></li>
+            <li><a  href="user_index.php?delete_user_acc" data-toggle="modal" data-target="#exampleModalauseracc">Delete Account</a></li>
+            <li ><a href="logout.php">Log out</a></li>
                </ul>
             </nav>
         </div>
--- user_area/user_new.php
+++ user_area/user_new.php
@@ -415,8 +415,16 @@ a{
 
 </style>
 
+    <!--boostrap css link-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
+    <!--font auwsom link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+<!--add CKEDITOR library-->
+<script src="https://cdn.ckeditor.com/4.16.2/full/ckeditor.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body style="background: #f0d5f5;">
