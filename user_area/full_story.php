7c00d820ee5497f3b70ea6414d946bd7854f3509
PathumiJayasiri <pathumijayasiri@gmail.com>
2023-11-24 10:47:50 +0530

    nav_story

--- user_area/full_story.php
+++ user_area/full_story.php
@@ -1,6 +1,7 @@
 <?php
 include('../DbConnector/connect.php');
 include('../functions/common_function.php');
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +11,7 @@ include('../functions/common_function.php');
     <title>story</title>
      <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
     <!--style css link-->
-<link rel="stylesheet" href="./style.css">
+<link rel="stylesheet" href=".. /style.css">
     
     <!--boostrap css link-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -361,7 +362,7 @@ echo "
 
  <!--story  section-->
  
-<h1 class="heading">all <span>stories</span> </h1>
+
 
  <!--row start-->
  <div class="row px-1">
